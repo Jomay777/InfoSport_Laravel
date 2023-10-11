26fbc9b49e1884820e09d4e53c7783401d83c4f4
Jomay777 <joelmamani1020@gmail.com>
2023-10-11 17:34:30 -0300

    Adding proyect with Laravel, Inertia and Jetstream

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Club;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        User::factory()->create([
+             'email' => 'i@admin.com',
+             'password' => bcrypt('123456')
+        ]);
+        Club::factory(10)->create();
+    }
+}
